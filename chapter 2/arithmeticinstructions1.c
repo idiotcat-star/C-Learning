f27eff258eea37f4b6372514b4e173b98c0f7789
Ranjeet <ranjeetbohora7@gmail.com>
2026-09-24 00:21:00 +0545

    Add arithmeticinstructions1.c to demonstrate basic arithmetic operations

---
+++ chapter 2/arithmeticinstructions1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+a = b = c;
+
+printf("the value of a is %d\n,the value of b is %d,the value of c is %d ",a,b,c);
+
+return 0;
+}
